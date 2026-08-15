58686d1d838f649cee9d5264bcf0551bd7a6240a
chatgptdameunnombre-png <chatgptdameunnombre@gmail.com>
2026-08-15 13:51:01 -0600

    cache-bust global v20 (imports versionados): js/db.js

--- js/db.js
+++ js/db.js
@@ -1,5 +1,5 @@
-import { firebaseConfig, usaFirebase } from "./config.js";
-import { PRODUCTOS_SEED } from "./seed.js";
+import { firebaseConfig, usaFirebase } from "./config.js?v=20";
+import { PRODUCTOS_SEED } from "./seed.js?v=20";
 
 const LS_KEY = "bici_productos";
 const LS_AUTH = "bici_auth";
